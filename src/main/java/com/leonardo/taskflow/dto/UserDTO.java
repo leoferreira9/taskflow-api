93087426fca7798288722cd9fca7d43ee891a616
Leonardo <leobarros_@outlook.com>
2025-11-11 20:22:05 -0300

    feat: add UserDTO

---
+++ src/main/java/com/leonardo/taskflow/dto/UserDTO.java
@@ -0,0 +1,58 @@
+package com.leonardo.taskflow.dto;
+
+import com.leonardo.taskflow.model.Role;
+import com.leonardo.taskflow.model.Task;
+import com.leonardo.taskflow.model.User;
+
+import java.util.List;
+
+public class UserDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private Role role;
+
+
+    public UserDTO(User user){
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.role = user.getRole();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
