1aac589533518c6c1ee5cbf94b0ca7ef221556d1
Leonardo <leobarros_@outlook.com>
2025-11-11 01:26:17 -0300

    feat: add UserService with CRUD operations and custom exception

---
+++ src/main/java/com/leonardo/taskflow/service/UserService.java
@@ -0,0 +1,50 @@
+package com.leonardo.taskflow.service;
+
+import com.leonardo.taskflow.exception.EntityNotFoundException;
+import com.leonardo.taskflow.model.User;
+import com.leonardo.taskflow.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User create(User user){
+        return userRepository.save(user);
+    }
+
+    public User findById(Long id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found with ID: " + id));
+    }
+
+    public List<User> findAll(){
+        List<User> users = userRepository.findAll();
+        if(users.isEmpty()) throw new EntityNotFoundException("No users registered!");
+        return users;
+    }
+
+    public User update(User user){
+        User existingUser = userRepository.findById(user.getId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found with ID: " + user.getId()));
+
+        existingUser.setName(user.getName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setRole(user.getRole());
+        existingUser.setPassword(user.getPassword());
+
+        return userRepository.save(existingUser);
+    }
+
+    public void delete(Long id){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found with ID: " + id));
+
+        userRepository.delete(user);
+    }
+}
