7d142a0b6dcb8f40306be22d5079beb15d9bde59
Leonardo <leobarros_@outlook.com>
2025-11-13 22:06:55 -0300

    feat: add nested endpoint POST /users/{id}/tasks with TaskCreateDTO

--- src/main/java/com/leonardo/taskflow/controller/UserController.java
+++ src/main/java/com/leonardo/taskflow/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.leonardo.taskflow.controller;
 
+import com.leonardo.taskflow.dto.TaskCreateDTO;
+import com.leonardo.taskflow.dto.TaskDTO;
 import com.leonardo.taskflow.dto.UserDTO;
 import com.leonardo.taskflow.dto.UserUpdateDTO;
+import com.leonardo.taskflow.model.Task;
 import com.leonardo.taskflow.model.User;
+import com.leonardo.taskflow.service.TaskService;
 import com.leonardo.taskflow.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +22,30 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private TaskService taskService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserDTO create(@RequestBody @Valid User user){
         User entity =  userService.create(user);
         return new UserDTO(entity);
+    }
+
+    @PostMapping("/{id}/tasks")
+    public TaskDTO createTaskForUser(@PathVariable Long id, @RequestBody TaskCreateDTO taskCreateDTO){
+        User user = userService.findById(id);
+        Task task = new Task();
+
+        task.setTitle(taskCreateDTO.getTitle());
+        task.setDueDate(taskCreateDTO.getDueDate());
+        task.setPriority(taskCreateDTO.getPriority());
+        task.setStatus(taskCreateDTO.getStatus());
+        task.setDescription(taskCreateDTO.getDescription());
+        task.setUser(user);
+
+        Task savedTask = taskService.create(task);
+        return new TaskDTO(savedTask);
     }
 
     @GetMapping("/{id}")
---
+++ src/main/java/com/leonardo/taskflow/dto/TaskCreateDTO.java
@@ -0,0 +1,63 @@
+package com.leonardo.taskflow.dto;
+
+import com.leonardo.taskflow.model.TaskPriority;
+import com.leonardo.taskflow.model.TaskStatus;
+
+import java.time.LocalDate;
+
+public class TaskCreateDTO {
+
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private TaskPriority priority;
+    private LocalDate dueDate;
+
+    public TaskCreateDTO(String title, String description, TaskStatus status, TaskPriority priority, LocalDate dueDate) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.dueDate = dueDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}
